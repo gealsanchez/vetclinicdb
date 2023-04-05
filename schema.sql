1c807c471f4189bd66909b605e6bec9a6e80fb91
gealsanchez <gealsanchez@gmail.com>
2023-04-05 11:51:36 -0500

    Query and update animals table

--- schema.sql
+++ schema.sql
@@ -1,9 +1,14 @@
 CREATE TABLE animals (
-    id              integer GENERATED ALWAYS AS IDENTITY,
-    name            varchar(40) not null,
-    date_of_birth   date not null,
+    id integer GENERATED ALWAYS AS IDENTITY,
+    name varchar(40) not null,
+    date_of_birth date not null,
     escape_attempts integer not null,
-    neutered        boolean not null,
-    weight_kg       decimal not null,
+    neutered boolean not null,
+    weight_kg decimal not null,
     PRIMARY KEY(id)
 );
+
+ALTER TABLE
+    animals
+ADD
+    species varchar(40);
